2cc5a626f340e074b79a006cd1f3665d11534bfc
anjul1026 <chaturvedianjul1899@gmail.com>
2023-07-16 05:01:07 -0700

    starting of newsMonky news app and adding some featurs in it

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
 
 export default class App extends Component {
   render() {
-    return <div>hi bhosdike</div>;
+    return (
+      <div>
+        <NavBar />
+        <News />
+      </div>
+    );
   }
 }
